cff5e6602084a37542b55c99171a88b461675dd9
Tanu <tanuvish3565@gmail.com>
2026-06-13 17:37:57 +0530

    Add Express server setup, MongoDB connection, and message/user models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,28 @@
 import express from "express"
 import "dotenv/config"
+import { connectDB } from "./lib/db.js"
+import { clerkMiddleware } from "@clerk/express"
+import cors from "cors"
 
 const app = express()
 const PORT = process.env.PORT
+const FRONTEND_URL = process.env.FRONTEND_URL
 
 
-app.listen(PORT,()=> console.log("server running"))
+app.use(express.json())
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+}))
+app.use(clerkMiddleware())
+
+app.get("/health", (req,res) => {
+    res.status(200).json({ ok: true })
+})
+
+app.listen(PORT,()=> {
+    console.log("server running")
+
+    connectDB()
+
+})
